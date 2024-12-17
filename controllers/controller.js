02fb065b573225643765bcae36fbe83a07ee56a6
Ekansh-Gahlot <60690453+Ekansh-Gahlot@users.noreply.github.com>
2024-12-17 14:06:44 +0530

    Added logic for different pages to update the info at any point

--- controllers/controller.js
+++ controllers/controller.js
@@ -16,25 +16,25 @@ async function displayGamePage(req,res){
 
 async function displayUpdateGame(req,res){
     try {
-        const {rows} = await await pool.query('Select * from games;');
+        const {rows} = await pool.query('Select * from games;');
         res.render('updateGameForm',{games:rows})
     } catch (error) {
         
     }
 }
 
+
 async function updateGame(req,res) {
     const {games} = req.body;
+    console.log(req.body)
     try {
         const queries = games.map(game=>{
             return pool.query( 'UPDATE games SET title = $1, release_date = $2, developer_id = $3, genre_id = $4 WHERE id = $5', [game.title,game.release_date, game.developer_id,game.genre_id,game.id]
             );
         });
-       
         await Promise.all(queries);
         console.log(queries);
         res.status(200).redirect('/');
-        // res.redirect('/');
     } catch (error) {
         console.error('Error Updating games',error);
         res.status(500).send('Error updating games')
@@ -42,14 +42,75 @@ async function updateGame(req,res) {
    
 }
 
-
-async function developerPages(req,res){
+async function displayUpdateDevelopers(req,res){
     try {
-        const {row} = await pool.query('Select * from developers');
-        console.log(row)
-        res.render('developersPages',{dev:row});
+        const {rows} = await pool.query('Select * from developers;');
+        res.render('updateDevelopersForm',{developers:rows})
     } catch (error) {
         
+    }
+}
+
+
+async function updateDevelopers(req,res){
+    // console.log(req.body)
+    const {developers} = req.body;
+    console.log(developers)
+    try{
+        const queries = developers.map(dev=>{
+            return pool.query('UPDATE developers SET name = $1, website = $2 WHERE id = $3', [dev.name, dev.website, dev.id]);
+        })
+        await Promise.all(queries)
+        // console.log(queries);
+        res.status(200).redirect('/');
+    }catch(error){
+        console.log('Error updating developers', error);
+        res.status(500).send('Error updating developers');
+    }
+}
+
+async function displayDeveloperPages(req,res){
+    try {
+        const {rows} = await pool.query('Select * from developers;');
+        res.render('developersPages',{devs:rows});
+    } catch (error) {
+        
+    }
+}
+
+async function displayGenres(req,res) {
+    try {
+        const {rows} = await pool.query('Select * from genres;')
+        res.render('genresPages', {genres:rows})
+    } catch (error) {
+        
+    }
+}
+
+
+async function displayUpdateGenres(req,res){
+    try {
+        const {rows} = await pool.query('Select * from genres;')
+        // console.log(rows);
+        res.render('updateGenreForm', {genres:rows})
+    } catch (error) {
+        
+    }
+}
+
+async function updateGenres(req,res){
+    try{
+        console.log('Hello display update genres')
+        const {genres} = req.body;
+        const queries = genres.map(genre=>{
+            return pool.query('UPDATE genres SET name = $1, description = $2 WHERE id = $3', [genre.name, genre.description, genre.id]);
+        })
+        await Promise.all(queries)
+        // console.log(queries);
+        res.status(200).redirect('/');
+    }catch(error){
+        console.log('Error updating genres', error);
+        res.status(500).send('Error updating genres');
     }
 }
 
@@ -57,6 +118,11 @@ module.exports = {
     displayHomePage,
     displayGamePage,
     displayUpdateGame,
-    developerPages,
-    updateGame
+    displayDeveloperPages,
+    updateGame,
+    updateDevelopers,
+    displayUpdateDevelopers,
+    displayGenres,
+    displayUpdateGenres,
+    updateGenres
 }
--- routes/appRouter.js
+++ routes/appRouter.js
@@ -10,6 +10,17 @@ appRouter.get('/update-games',appController.displayUpdateGame);
 
 appRouter.post('/update-games', appController.updateGame)
 
-appRouter.get('/developers',appController.developerPages)
+appRouter.get('/developers',appController.displayDeveloperPages)
+
+appRouter.get('/update-developers',appController.displayUpdateDevelopers)
+
+appRouter.post('/update-developers',appController.updateDevelopers)
+
+appRouter.get('/genres', appController.displayGenres)
+
+appRouter.get('/update-genres', appController.displayUpdateGenres)
+
+appRouter.post('/update-genres', appController.updateGenres)
 
 module.exports = appRouter;
+
